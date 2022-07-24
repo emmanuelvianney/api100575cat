0af0499408b5cc601bcb0f3df4bb0710d7b48971
emmanuelvianney <evmungai@gmail.com>
2022-07-24 20:08:43 +0300

    first commit

--- src/main/java/com/apps/lostcard/LostcardApplication.java
+++ src/main/java/com/apps/lostcard/LostcardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LostcardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LostcardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LostcardApplication.class, args);
+    }
 
 }
